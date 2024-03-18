d47635a84fecad934087aa3e24243c115b6af69c
MarcoColloca <marco.colloca3511@gmail.com>
2024-03-18 14:53:06 +0100

    Add → added static array of objects with datas

--- js/app.js
+++ js/app.js
@@ -1,1 +1,43 @@
 console.log('JS Test')
+
+
+
+
+const teamArray = [
+    // 1° membro del Team
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        img: 'wayne-barnett-founder-ceo.jpg'
+    },
+    // 2° membro del Team
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        img: 'angela-caroll-chief-editor.jpg'
+    },
+    // 3° membro del Team
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        img: ' walter-gordon-office-manager.jpg'
+    },
+    // 4° membro del Team
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        img: 'angela-lopez-social-media-manager.jpg'
+    },
+    // 5° membro del Team
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        img: 'scott-estrada-developer.jpg'
+    },
+    // 6° membro del Team
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        img: 'barbara-ramos-graphic-designer.jpg'
+    }
+]   
