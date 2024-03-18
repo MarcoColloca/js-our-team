edb4cd33bd782b9e78b60622fc16e455f81926de
MarcoColloca <marco.colloca3511@gmail.com>
2024-03-18 15:45:45 +0100

    Feat → created function for bootstrap card generation and printed  dynamic cards in  HTML

--- js/app.js
+++ js/app.js
@@ -1,6 +1,36 @@
 console.log('JS Test')
 
 
+function createBootstrapCard(cardName, cardRole, cardImg){
+    const bootstrapCard = document.createElement('div')
+    const bootstrapCardImg = document.createElement('img')
+    const bootstrapCardBody = document.createElement('div')
+    const bootstrapCardBodyTitle = document.createElement('h5')
+    const bootstrapCardBodyText = document.createElement('p')
+
+    // Card
+    bootstrapCard.classList.add('card')
+
+    // Card Img
+    bootstrapCardImg.src = cardImg
+
+    // Card Body
+    bootstrapCardBody.classList.add('card-body')
+
+    // Card Title
+    bootstrapCardBodyTitle.classList.add('card-title')
+    bootstrapCardBodyTitle.innerHTML = cardName
+
+    bootstrapCardBodyText.classList.add('card-text')
+    bootstrapCardBodyText.innerHTML = cardRole
+
+    // Card Title
+    cardsDOMElement.append(bootstrapCard)
+    bootstrapCard.append(bootstrapCardImg, bootstrapCardBody)
+    bootstrapCardBody.append(bootstrapCardBodyTitle, bootstrapCardBodyText)
+}
+
+
 
 
 
@@ -12,37 +42,37 @@ const teamArray = [
     {
         name: 'Wayne Barnett ',
         role: 'Founder & CEO',
-        img: '<img src="./img/wayne-barnett-founder-ceo.jpg">'
+        img: './img/wayne-barnett-founder-ceo.jpg'
     },
     // 2° membro del Team
     {
         name: 'Angela Caroll ',
         role: 'Chief Editor',
-        img: '<img src="./img/angela-caroll-chief-editor.jpg">'
+        img: './img/angela-caroll-chief-editor.jpg'
     },
     // 3° membro del Team
     {
         name: 'Walter Gordon ',
         role: 'Office Manager',
-        img: '<img src="./img/walter-gordon-office-manager.jpg">'
+        img: './img/walter-gordon-office-manager.jpg'
     },
     // 4° membro del Team
     {
         name: 'Angela Lopez ',
         role: 'Social Media Manager',
-        img: '<img src="./img/angela-lopez-social-media-manager.jpg">'
+        img: './img/angela-lopez-social-media-manager.jpg'
     },
     // 5° membro del Team
     {
         name: 'Scott Estrada ',
         role: 'Developer',
-        img: '<img src="./img/scott-estrada-developer.jpg">'
+        img: './img/scott-estrada-developer.jpg'
     },
     // 6° membro del Team
     {
         name: 'Barbara Ramos ',
         role: 'Graphic Designer',
-        img: '<img src="./img/barbara-ramos-graphic-designer.jpg">'
+        img: './img/barbara-ramos-graphic-designer.jpg'
     }
 ]   
 
@@ -61,10 +91,8 @@ for(let i = 0; i < teamArray.length; i++){
     console.log('Ruolo: ' + teamMember.role)
     console.log('Immagine: ' + teamMember.img)
     //cardsDOMElement.append(teamMember.name, teamMember.role, teamMember.img)
-    const myCardDOMElement = document.createElement('div')
-    myCardDOMElement.classList.add('card', 'col-6', 'd-flex', 'justify-content-between')
-    myCardDOMElement.innerHTML = teamMember.name + teamMember.role + teamMember.img
-    cardsDOMElement.append(myCardDOMElement)
+    
+    createBootstrapCard(teamMember.name, teamMember.role, teamMember.img)
 }
 
 /*
